5ddb623d0a6fdad7bb4d929414159ab22585b7c7
abUwUser <pedro.santos.cartaxo@gmail.com>
2020-12-07 11:26:37 -0300

    some advice about its instability

--- components/Settings.jsx
+++ components/Settings.jsx
@@ -1,7 +1,7 @@
 'use strict'
 
 const { React } = require('@vizality/webpack')
-const { SwitchItem } = require('@vizality/components/settings')
+const { settings: { SwitchItem }, FormNotice, KeyboardShortcut } = require('@vizality/components')
 
 const PluginList = require('./PluginList.jsx')
 
@@ -13,6 +13,18 @@ module.exports = class Settings extends React.Component {
   render () {
     return (
       <div>
+        <FormNotice
+          imageData={{
+            width: 60,
+            height: 60,
+            src: '/assets/0694f38cb0b10cc3b5b89366a0893768.svg'
+          }}
+          type={FormNotice.Types.WARNING}
+          title={"Plugin in development"}
+          body={
+            <p>This plugin is in development stages and it contains a lot of bugs that're trying to fix it. Please be patient and expect bugs. Also maybe you see some perfomance issues. When you see that happening, please refresh Discord by pressing this keybind: <KeyboardShortcut shortcut="mod+r" /></p>
+          }
+        /><br></br><br></br>
         <SwitchItem value={this.props.getSetting('disableWhenStopFailed')}
         onChange={() => this.props.toggleSetting('disableWhenStopFailed')}>
           Disable plugin when failed to stop
