56a59997591944884b42e29e66d2afd64862ec16
abUwUser <pedro.santos.cartaxo@gmail.com>
2020-12-11 02:05:24 -0300

    ❤️

--- components/Settings.jsx
+++ components/Settings.jsx
@@ -1,11 +1,13 @@
 'use strict'
 
-const { React, i18n: { Messages } } = require('@vizality/webpack')
+const { React, i18n: { Messages }, getModule, getModuleByDisplayName } = require('@vizality/webpack')
 const { FormNotice, Anchor, Button, Divider } = require('@vizality/components')
 const { open: openModal } = require('@vizality/modal')
 
 const PluginList = require('./PluginList.jsx')
 const SettingsModal = require('./modals/PluginSettingsModal.jsx')
+
+const Image = getModuleByDisplayName("Image")
 
 module.exports = class Settings extends React.Component {
   constructor (props) {
@@ -25,6 +27,7 @@ module.exports = class Settings extends React.Component {
           title={Messages.BDCOMAPT_INDEV_NOTICE.title}
           body={<div>{Messages.BDCOMAPT_INDEV_NOTICE.description} <Anchor href="https://github.com/A-User-s-Discord-Plugins/bdCompat#faq">{Messages.BDCOMAPT_INDEV_NOTICE.openFAQ}</Anchor></div>}
         /><br></br><br></br>
+
         <div className="vzbdcompat-horizontal">
           <Button
             onClick={() => openModal(() => <SettingsModal stuff={this.props} />)}
@@ -48,6 +51,15 @@ module.exports = class Settings extends React.Component {
         <br />
 
         <PluginList pluginManager={window.pluginModule} settings={this.props} />
+
+        <div className="description-3_Ncsb vzbdcompat-horizontal vzbdcompat-thanks">
+          Ported with 
+          <Image
+            width= {25}
+            height= {25}
+            src= "/assets/0483f2b648dcc986d01385062052ae1c.svg"
+            zoomable= {false}
+        /> by A user</div>
       </div>
     )
   }
